9e2788a30ed13698f8a0e65e5dbc2628ecf9abcc
Pavan Naregundi <pnaregundi@marvell.com>
2023-08-11 05:33:19 +0000

    Preparing for tag - master_03
    Signed-off-by: Pavan Naregundi <pnaregundi@marvell.com>

--- sonic_master_apply_patch_bullseye.sh
+++ sonic_master_apply_patch_bullseye.sh
@@ -24,7 +24,7 @@ LOG_FILE=patches_result.log
 FULL_PATH=`pwd`
 
 # Path for master patches
-WGET_PATH="https://raw.githubusercontent.com/Marvell-switching/sonic-scripts/master/files/master-bullseye/"
+WGET_PATH="https://raw.githubusercontent.com/Marvell-switching/sonic-scripts/master_03/files/master-bullseye/"
 
 # Patches
 SERIES="0001-Marvell-arm64-Support-for-lazy-install-sdk-drivers.patch
@@ -121,7 +121,7 @@ apply_submodule_patches()
 apply_hwsku_changes()
 {
     # Download hwsku
-    wget -c https://raw.githubusercontent.com/Marvell-switching/sonic-scripts/master/files/mrvl_sonic_hwsku_ezb.tgz
+    wget -c https://raw.githubusercontent.com/Marvell-switching/sonic-scripts/master_03/files/mrvl_sonic_hwsku_ezb.tgz
 
     rm -fr device/marvell/x86_64-marvell_db98cx8580_32cd-r0 || true
     rm -rf device/marvell/x86_64-marvell_slm5401_54x-r0     || true
