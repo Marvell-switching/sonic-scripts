09f6e1bd6f3d1c9395abe6585c252ce197670c56
Pavan Naregundi <pnaregundi@marvell.com>
2024-04-12 07:32:39 +0000

    202311: build script update
    Signed-off-by: Pavan Naregundi <pnaregundi@marvell.com>

--- sonic_202311_apply_patch_bullseye.sh
+++ sonic_202311_apply_patch_bullseye.sh
@@ -24,7 +24,7 @@ LOG_FILE=patches_result.log
 FULL_PATH=`pwd`
 
 # Path for 202311 patches
-WGET_PATH="https://raw.githubusercontent.com/Marvell-switching/sonic-scripts/master/files/202311/"
+WGET_PATH="https://raw.githubusercontent.com/Marvell-switching/sonic-scripts/202311_02/files/202311/"
 
 # Patches
 SERIES="0001-Redis-timeout-WA.patch
@@ -122,11 +122,33 @@ apply_submodule_patches()
 apply_hwsku_changes()
 {
     # Download hwsku
-    wget --timeout=2 -c https://raw.githubusercontent.com/Marvell-switching/sonic-scripts/master/files/mrvl_sonic_hwsku_ezb.tgz
+    wget --timeout=2 -c https://raw.githubusercontent.com/Marvell-switching/sonic-scripts/202311_02/files/mrvl_sonic_hwsku_ezb.tgz
 
+    rm -fr device/marvell/x86_64-marvell_db98cx8580_32cd-r0 || true
+    rm -rf device/marvell/x86_64-marvell_slm5401_54x-r0     || true
+    rm -fr device/marvell/arm64-marvell_db98cx8580_32cd-r0  || true
+    rm -fr device/marvell/x86_64-marvell_db98cx8540_16cd-r0 || true
+    rm -fr device/marvell/arm64-marvell_db98cx8540_16cd-r0  || true
+    rm -fr device/marvell/armhf-marvell_et6448m_52x-r0      || true
     tar -C device/marvell/ -xzf mrvl_sonic_hwsku_ezb.tgz
+    cp -dr device/marvell/arm64-marvell_db98cx8580_32cd-r0 device/marvell/x86_64-marvell_db98cx8580_32cd-r0
+    cp -dr device/marvell/arm64-marvell_db98cx8540_16cd-r0 device/marvell/x86_64-marvell_db98cx8540_16cd-r0
+    cp -dr device/marvell/arm64-marvell_db98cx8514_10cc-r0 device/marvell/x86_64-marvell_db98cx8514_10cc-r0
+    rm device/marvell/arm64-marvell_db98cx8580_32cd-r0/plugins/x86_64_sfputil.py
+    rm device/marvell/arm64-marvell_db98cx8540_16cd-r0/plugins/x86_64_sfputil.py
+    rm device/marvell/arm64-marvell_db98cx8514_10cc-r0/plugins/x86_64_sfputil.py
+    mv device/marvell/x86_64-marvell_db98cx8580_32cd-r0/plugins/x86_64_sfputil.py device/marvell/x86_64-marvell_db98cx8580_32cd-r0/plugins/sfputil.py
+    mv device/marvell/x86_64-marvell_db98cx8540_16cd-r0/plugins/x86_64_sfputil.py device/marvell/x86_64-marvell_db98cx8540_16cd-r0/plugins/sfputil.py
+    mv device/marvell/x86_64-marvell_db98cx8514_10cc-r0/plugins/x86_64_sfputil.py device/marvell/x86_64-marvell_db98cx8514_10cc-r0/plugins/sfputil.py
+    echo "marvell" > device/marvell/x86_64-marvell_db98cx8514_10cc-r0/platform_asic
+    echo "marvell" > device/marvell/x86_64-marvell_db98cx8540_16cd-r0/platform_asic
+    echo "marvell" > device/marvell/x86_64-marvell_db98cx8580_32cd-r0/platform_asic
+    echo "marvell" > device/marvell/arm64-marvell_db98cx8514_10cc-r0/platform_asic
+    echo "marvell" > device/marvell/arm64-marvell_db98cx8540_16cd-r0/platform_asic
+    echo "marvell" > device/marvell/arm64-marvell_db98cx8580_32cd-r0/platform_asic
     echo "marvell-arm64" > device/marvell/arm64-marvell_rd98DX35xx-r0/platform_asic
     echo "marvell-arm64" > device/marvell/arm64-marvell_rd98DX35xx_cn9131-r0/platform_asic
+    echo "marvell" > device/marvell/x86_64-marvell_rd98DX35xx-r0/platform_asic
 }
 
 main()
