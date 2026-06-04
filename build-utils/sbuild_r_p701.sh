32704c0c959848ed8b4e05a97bd759f3b2658532
Yan Markman <ymarkman@marvell.com>
2026-06-04 20:09:09 +0300

    script: separate release build-configuration
    Signed-off-by: Yan Markman <ymarkman@marvell.com>

--- build-utils/sbuild_r_p701.sh
+++ build-utils/sbuild_r_p701.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
+################################################################
+##############################################################
+# <<  Release build-configuration
+SONIC_BRANCH=202511
+SONIC_COMMIT_ID=922fae553
+MRVLLIBSAI_VER=1.17.1-21
+##CANONIC_BUILD=Y
+MV_EXTRA_PATCH_SETS=Y
+# >>
+# echo/print is at the end
+##############################################################
+################################################################
+
+rBUILD_PLATFORM="-p marvell-prestera"
+rBUILD_RPC="-r"
+#rBUILD_SAISERVER="-s"
+rBUILD_NO_CACHE="--no-cache"
+
+if [ -n "${SONIC_BRANCH}" ]; then
+    rBRANCH="-b $SONIC_BRANCH"
+fi
+if [ -n "${SONIC_COMMIT_ID}" ]; then
+    rCOMMIT_ID="-c $SONIC_COMMIT_ID"
+fi
+if [ -z "${CANONIC_BUILD}" ]; then
+    rPATCH_SCRIPT_PATH="--patch_script `pwd`/marvell_sonic_patch_script.sh"
+fi
+
 SONIC_TARGET_ARCH="${SONIC_TARGET_ARCH:-$(uname -m)}"
   case "$SONIC_TARGET_ARCH" in
     x86_64|i386|i686)  rARCH="amd64"  ;;
@@ -9,28 +37,20 @@ SONIC_TARGET_ARCH="${SONIC_TARGET_ARCH:-$(uname -m)}"
   esac
 
 rBUILD_PLATFORM_ARCH="-a $rARCH"
-rBRANCH="-b 202511"
-rBUILD_PLATFORM="-p marvell-prestera"
-rBUILD_RPC="-r"
-#rBUILD_SAISERVER="-s"
-rBUILD_NO_CACHE="--no-cache"
-rCOMMIT_ID="-c 922fae553"
-SAI_VER=1.17.1-21
 
-rPATCH_SCRIPT_PATH="--patch_script `pwd`/marvell_sonic_patch_script.sh"
-#rSAI_DEB="--SAI `pwd`/mrvllibsai_1.17.1-21_${rARCH}.deb"
-#rSAI_DEB="--SAI <URL>/mrvllibsai_1.17.1-21_${rARCH}.deb"
-
-if [ -z "${rSAI_DEB}" ]; then
-    rSAI_VER="--SAI_VER ${SAI_VER}"
-    if [ -f "./mrvllibsai_${SAI_VER}_${rARCH}.deb" ]; then
-        rSAI_DEB="--SAI `pwd`/mrvllibsai_${SAI_VER}_${rARCH}.deb"
+#rSAI_DEB="--SAI `pwd`/mrvllibsai_1.17.1-nn_${rARCH}.deb"
+#rSAI_DEB="--SAI <URL>/mrvllibsai_1.17.1-nn_${rARCH}.deb"
+#---------
+if [[ -z "${rSAI_DEB}" && -n ${MRVLLIBSAI_VER} ]]; then
+    rSAI_VER="--SAI_VER ${MRVLLIBSAI_VER}"
+    if [ -f "./mrvllibsai_${MRVLLIBSAI_VER}_${rARCH}.deb" ]; then
+        rSAI_DEB="--SAI `pwd`/mrvllibsai_${MRVLLIBSAI_VER}_${rARCH}.deb"
     fi
 fi
 
 rEXTRA_OPT=$@
 
-if [ "$rARCH" == "amd64" ]; then
+if [[ -n $MV_EXTRA_PATCH_SETS && "$CANONIC_BUILD" != "Y" && "$rARCH" == "amd64" ]]; then
 # patches from    "-1  -2  -3"
 rEXTRA_PATCH_SETS="-1"
 fi
@@ -42,6 +62,13 @@ SONIC_BUILD_SH_CMD="./sonic_build_script.sh $rBRANCH $rBUILD_PLATFORM $rBUILD_PL
  $rPATCH_SCRIPT_PATH $rSAI_VER $rSAI_DEB $rEXTRA_OPT"
 
 echo "========================================================================="
+echo "<< Release build-configuration"
+echo "SONIC_BRANCH=$SONIC_BRANCH"
+echo "SONIC_COMMIT_ID=$SONIC_COMMIT_ID"
+echo "MRVLLIBSAI_VER=$MRVLLIBSAI_VER"
+echo "CANONIC_BUILD=$CANONIC_BUILD"
+echo "MV_EXTRA_PATCH_SETS=$rEXTRA_PATCH_SETS"
+echo ">>"
 echo "Run in directory `pwd`"
 echo "$SONIC_BUILD_SH_CMD"
 echo "-------------------------------------------------------------------------"
